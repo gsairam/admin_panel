b0072c2a6a41a7ecbea3037c99cc4693c396c89c
sairam <ganugula.sairam@gmail.com>
2016-09-14 12:07:10 +0530

    validate

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,16 +7,34 @@
  * # MainCtrl
  * Controller of yapp
  */
-angular.module('yapp')
-  .controller('LoginCtrl', function($scope, $location) {
 
-    console.log('ok');
+ var adminUrl = 'users/',
+ baseUrl = '/1/objects/';
+
+
+angular.module('yapp')
+  .controller('LoginCtrl', function($scope,$http,$location,Backand,UserService) {
+
+    function getAdminUrl() {
+      return Backand.getApiUrl() + baseUrl + adminUrl;
+    };
 
     $scope.submit = function() {
+      $http.get(getAdminUrl())
+      .then(function(result){
+         $scope.loginData = result.data.data;
+         console.log($scope.loginData);
+         if($scope.loginData[0].username==$scope.logdata.user && $scope.loginData[0].password==$scope.logdata.password)
+          {
+            $location.path('/dashboard');
+            return false;
+          }
+          else {
+            $scope.message="Incorrect username or password";
+          }
+      });
 
-      $location.path('/dashboard');
 
-      return false;
     };
 
   });
